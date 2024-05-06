9ed303848160a27c3a5493dc5d4f0c1a0272427b
Erik <mirkovic.erik@gmail.com>
2024-05-06 13:48:32 +0200

    format

--- social/contrib/sites/migrations/0001_initial.py
+++ social/contrib/sites/migrations/0001_initial.py
@@ -33,9 +33,7 @@ class Migration(migrations.Migration):
                 ),
                 (
                     "name",
-                    models.CharField(
-                        max_length=50, verbose_name="display name"
-                    ),
+                    models.CharField(max_length=50, verbose_name="display name"),
                 ),
             ],
             options={
